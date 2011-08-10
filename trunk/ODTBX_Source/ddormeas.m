d36a60a9f514dc7a02cc1743565521fabdc1fc4b
Kevin Berry <kevin.e.berry@nasa.gov>
2011-08-10 19:08:02 +0000

    Issue 372: Update ddormeas.m Help Added help statement about restarting the quasar assignment with a "clear ddormeas.m" command. Previous commit was incomplete.

--- trunk/ODTBX_Source/ddormeas.m
+++ trunk/ODTBX_Source/ddormeas.m
@@ -6,9 +6,11 @@ function [y,H,R] = ddormeas(t,x,options)
 % type that this function returns is an interferometric range difference
 % with that of a "quasar". Rather than use an actual quasar database, the
 % "quasar" used in this model is assumed to be directly behind the
-% satellite at t=0 and fixed in ECI space for all times after. To reset the
-% "quasar" position, add a "clear ddormeas.m" statement to clear the
-% persistent variable that was holding the ECI unit vector.
+% satellite at the first time step and fixed in ECI space for all times
+% after. To reset the "quasar" position, add a "clear ddormeas.m" statement
+% to clear the persistent variable that was holding the ECI unit vector.
+% This function will then recalculate it based on the satellite position at
+% the first time step of the next call.
 %
 %   INPUTS
 %   VARIABLE        SIZE    DESCRIPTION (Optional/Default)

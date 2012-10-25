70ab4aa32a1632b128199bf572b2b6a528ffdce4
Phillip Anderson <phillip.anderson@emergentspace.com>
2012-10-25 17:51:02 -0600

    Added an About menu that brings up my contact information.

--- meas_sched.m
+++ meas_sched.m
@@ -178,6 +178,7 @@ function about_Callback(hObject, eventdata, handles)
     % hObject    handle to about (see GCBO)
     % eventdata  reserved - to be defined in a future version of MATLAB
     % handles    structure with handles and user data (see GUIDATA)
+    about('meas_sched', handles.figure1);
 end
 
 

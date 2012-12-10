70dbfda8505935a91f3da0d898efee60bd135630
Ravi Mathur <ravi.mathur@emergentspace.com>
2012-12-10 17:21:30 -0500

    Mantis 407: Remove GMAT_Linux64 folder since GMAT is now available externally.

--- vendor/GMAT_Linux64/GMAT/bin/RunGmat.sh
+++
@@ -1,4 +0,0 @@
-# Linux launcher configured for use from the bin folder
-# Update the path information here to match your MATLAB location
-LD_LIBRARY_PATH=.:./lib:/opt/MATLAB/R2011a/bin/glnxa64 ./GMAT
-
--- vendor/GMAT_Linux64/GMAT/bin/RunGmatLauncher.sh
+++
@@ -1,4 +0,0 @@
-# Linux launcher configured for use with a desktop icon
-# Update the path information here to match your configuration
-cd ~/TS_Code/GmatDevelopment/application/bin;LD_LIBRARY_PATH=.:./lib:/opt/MATLAB/R2011a/bin/glnxa64 ./GMAT
-

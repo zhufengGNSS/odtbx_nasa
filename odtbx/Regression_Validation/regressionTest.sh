c24e5176998df051868e6facf8ef79667cd4be13
ODTBX test user <odtbx@GS-MESA2550471S.ndc.nasa.gov>
2012-12-13 16:58:19 -0500

    Regression test script can now be run manually. Custom email and custom MATLAB function can be specified. Errors during Git merging are emailed correctly.

--- odtbx/Regression_Validation/regressionTest.sh
+++ odtbx/Regression_Validation/regressionTest.sh
@@ -7,12 +7,11 @@
 # then the regression test is not run.
 #
 # Arguments: 
-# -f         Force-run the regression test, even if nothing changed in
+# [-f]       Force-run the regression test, even if nothing changed in
 #            the develop branch
-# (optional) The recipient's email address, if not provided then
-#            the DEFAULT_EMAIL below is used
-# (optional) The email server to use, if not provided then
-#            the DEFAULT_SMTP_SERVER below is used
+# [-e email] The recipient's email address (default odtbx@lists.nasa.gov)
+# [-s smtp]  The smtp server to use (default mailhost.gsfc.nasa.gov)
+# [-m cmd]   Run the specified MATLAB command verbatim (default regressionTesting)
 #
 # ODTBX: Orbit Determination Toolbox
 # 
@@ -35,54 +34,77 @@ function send_email
   subject=$2
   message=$3
   server=$4
+  attachfile=$5
 
   matlab -nodisplay -r "\
     setpref('Internet','SMTP_Server','$server');\
     setpref('Internet','E_mail','odtbx-builder@emergentspace.com');\
-    sendmail('$email','$subject','$message');\
+    sendmail('$email','$subject','$message','$attachfile');\
     quit;"
 }
-
-# Set these to enable email, if no email is desired, then set each to
-# ''.
-# keb - send mail to the nasa mail list
-DEFAULT_EMAIL='odtbx@lists.nasa.gov'
-DEFAULT_SMTP_SERVER='mailhost.gsfc.nasa.gov'
-
-export LANG=en_US.UTF-8
-
-# Set outgoing email address if specified
-if [ -n "$1" ] ; then
-        EMAIL=$1
-else
-        EMAIL=$DEFAULT_EMAIL
-fi
-
-# Set outgoing email server if specified
-if [ -n "$2" ] ; then
-        SMTP_SERVER=$2
-else
-        SMTP_SERVER=$DEFAULT_SMTP_SERVER
-fi
 
 # Add paths to built-in commands (e.g. 'date' and 'tee')
 PATH=/usr/bin:/bin:$HOME/bin
 
+export LANG=en_US.UTF-8
+
+# Set paths to needed files/folders
 ODTBX="$HOME/projects/odtbxsync-git/odtbx"
-echo "ODTBX Directory = $ODTBX"
-
 VENDOR="$HOME/projects/odtbxsync-git/vendor"
-echo "ODTBX Vendor Directory = $VENDOR"
-
 LOG_PATH="$HOME/log"
 INFO_LOG_FILE="$LOG_PATH/supporting_info.txt"
 
-echo "ODTBX Regression Test update,build,run script running at `date`" | tee $INFO_LOG_FILE
+echo \e "\nODTBX Regression Test script running on `date`" | tee $INFO_LOG_FILE
+echo "ODTBX Directory = $ODTBX"
+echo "ODTBX Vendor Directory = $VENDOR"
+
+# Set default variable values
+EMAIL="odtbx@lists.nasa.gov"
+SMTP_SERVER="mailhost.gsfc.nasa.gov"
+FORCE=false
+MATLABCMD="regressionTesting('$LOG_PATH','$EMAIL','$SMTP_SERVER','$INFO_LOG_FILE')"
+
+# Parse input arguments
+USAGE="Usage: regressionTest.sh [-f] [-h] [-e emailaddr] [-s smtpserver] [-m matlabcmd]"
+while getopts fhe:s:m: OPT; do
+	case "$OPT" in
+	f)
+		FORCE=true
+		;;
+	h)
+		echo $USAGE
+		exit 0
+		;;
+	e)
+		EMAIL=$OPTARG
+		;;
+	s)
+		SMTP_SERVER=$OPTARG
+		;;
+	m)
+		MATLABCMD="$OPTARG"
+		;;
+	\?)
+		# getopts issues an error message
+		echo $USAGE | tee -a $INFO_LOG_FILE
+		exit 1
+		;;
+	esac
+done
+shift `expr $OPTIND - 1`
+
+# Make sure there are no other arguments
+if [ $# != 0 ]; then
+	echo "Invalid input arguments to regressionTest.sh"
+	echo $USAGE | tee -a $INFO_LOG_FILE
+	exit 1
+fi
 
 # rmathur
 # The process of merging public and internal Git repositories is documented
 # in ~/projects/README-GIT.txt .
 git_subj="ODTBX Git failure"
+git_msg="There was an error while merging Git repositories. See the attached log."
 
 # Switch to the master branch, so that changes to master can be 
 # properly merged
@@ -90,9 +112,9 @@ echo -e "\n***Switching to master branch." | tee -a $INFO_LOG_FILE
 git checkout master >> $INFO_LOG_FILE 2>&1
 gitstatus=$? # Get error code of git checkout master
 if [ $gitstatus != 0 ]; then
-	msg="git checkout master failed!"
+	msg="git checkout master failed! Git status = $gitstatus"
 	echo $msg >> $INFO_LOG_FILE
-	send_email $EMAIL $git_subj $msg $SMTP_SERVER
+	send_email $EMAIL "$git_subj" "$git_msg" $SMTP_SERVER "$INFO_LOG_FILE"
 	exit $gitstatus
 fi
 
@@ -101,9 +123,9 @@ echo -e "\n***Pulling internal master branch." | tee -a $INFO_LOG_FILE
 git pull internal master >> $INFO_LOG_FILE 2>&1
 gitstatus=$? # Get error code of git pull
 if [ $gitstatus != 0 ]; then
-	msg="git pull internal master failed!"
+	msg="git pull internal master failed! Git status = $gitstatus"
 	echo $msg >> $INFO_LOG_FILE
-	send_email $EMAIL $git_subj $msg $SMTP_SERVER
+	send_email $EMAIL "$git_subj" "$git_msg" $SMTP_SERVER "$INFO_LOG_FILE"
 	exit $gitstatus
 fi
 
@@ -112,9 +134,9 @@ echo -e "\n***Pulling public master branch." | tee -a $INFO_LOG_FILE
 git pull public master >> $INFO_LOG_FILE 2>&1
 gitstatus=$? # Get error code of git pull
 if [ $gitstatus != 0 ]; then
-	msg="git pull public master failed!"
+	msg="git pull public master failed! Git status = $gitstatus"
 	echo $msg >> $INFO_LOG_FILE
-	send_email $EMAIL $git_subj $msg $SMTP_SERVER
+	send_email $EMAIL "$git_subj" "$git_msg" $SMTP_SERVER "$INFO_LOG_FILE"
 	exit $gitstatus
 fi
 
@@ -129,9 +151,9 @@ echo -e "\n***Switching to develop branch." | tee -a $INFO_LOG_FILE
 git checkout develop >> $INFO_LOG_FILE 2>&1
 gitstatus=$? # Get error code of git checkout develop
 if [ $gitstatus != 0 ]; then
-	msg="git checkout develop failed!"
+	msg="git checkout develop failed! Git status = $gitstatus"
 	echo $msg >> $INFO_LOG_FILE
-	send_email $EMAIL $git_subj $msg $SMTP_SERVER
+	send_email $EMAIL "$git_subj" "$git_msg" $SMTP_SERVER "$INFO_LOG_FILE"
 	exit $gitstatus
 fi
 
@@ -144,9 +166,9 @@ echo -e "\n***Pulling internal develop branch." | tee -a $INFO_LOG_FILE
 git pull internal develop >> $INFO_LOG_FILE 2>&1
 gitstatus=$? # Get error code of git pull
 if [ $gitstatus != 0 ]; then
-	msg="git pull internal develop failed!"
+	msg="git pull internal develop failed! Git status = $gitstatus"
 	echo $msg >> $INFO_LOG_FILE
-	send_email $EMAIL $git_subj $msg $SMTP_SERVER
+	send_email $EMAIL "$git_subj" "$git_msg" $SMTP_SERVER "$INFO_LOG_FILE"
 	exit $gitstatus $gitstatus
 fi
 
@@ -155,9 +177,9 @@ echo -e "\n***Pulling public develop branch." | tee -a $INFO_LOG_FILE
 git pull public develop >> $INFO_LOG_FILE 2>&1
 gitstatus=$? # Get error code of git pull
 if [ $gitstatus != 0 ]; then
-	msg="git pull public develop failed!"
+	msg="git pull public develop failed! Git status = $gitstatus"
 	echo $msg >> $INFO_LOG_FILE
-	send_email $EMAIL $git_subj $msg $SMTP_SERVER
+	send_email $EMAIL "$git_subj" "$git_msg" $SMTP_SERVER "$INFO_LOG_FILE"
 	exit $gitstatus
 fi
 
@@ -168,7 +190,7 @@ echo -e "\nAfter merge, develop commit = $nextcommit" | tee -a $INFO_LOG_FILE
 # If the develop branch commit hash didn't change after merging, then
 # nothing was done during the merge, so there's nothing to test.
 # But if the "-f" flag was given, then always run the regression test.
-if [ "$prevcommit" == "$nextcommit" ] && [ "$1" != "-f" ]; then
+if [ "$prevcommit" == "$nextcommit" ] && ! $FORCE ; then
 	echo "Branch develop has not changed. Nothing to test." >> $INFO_LOG_FILE
 	exit 0
 fi
@@ -183,9 +205,9 @@ echo -e "\nBuilding JAT with Apache Maven." | tee -a $INFO_LOG_FILE
 mvn -f "$VENDOR/Jat/maven/pom.xml" clean compile >> $INFO_LOG_FILE 2>&1
 mvnstatus=$? # Get error code of mvn compile
 if [ $mvnstatus != 0 ]; then
-	msg="mvn clean compile failed!"
+	msg="mvn clean compile failed! Maven status = $mvnstatus"
 	echo $msg >> $INFO_LOG_FILE
-	send_email $EMAIL "ODTBX Maven failure" $msg $SMTP_SERVER
+	send_email $EMAIL "ODTBX Maven failure" "There was an error while building JAT with Maven. See the attached log." $SMTP_SERVER "$INFO_LOG_FILE"
 	exit $mvnstatus
 fi
 
@@ -195,5 +217,5 @@ matlab -nodisplay -r "\
         basePath = '$ODTBX';\
         addpath(basePath);\
         startup();\
-        regressionTesting('$LOG_PATH','$EMAIL','$SMTP_SERVER','$INFO_LOG_FILE');\
+	$MATLABCMD;\
         quit;"

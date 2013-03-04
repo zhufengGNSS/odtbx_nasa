de1f41366dc751f19431867b10ec044f552a9ce2
Phillip Anderson <phillip.anderson@emergentspace.com>
2013-03-04 14:33:46 -0700

    Added new control class.

---
+++ odtbx/ODTBX_Source/est_control.m
@@ -0,0 +1,12 @@
+classdef est_control < estnew
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+

e97234dc886a2eebc44858a448342aee743c50c7
Phillip Anderson <phillip.anderson@emergentspace.com>
2013-04-24 23:21:51 -0600

    Made variables structures, added in external forces function stub.

--- odtbx/ODTBX_Source/solve_consider.m
+++ odtbx/ODTBX_Source/solve_consider.m
@@ -3,43 +3,49 @@ classdef solve_consider
     %   Detailed explanation goes here
     
     properties
-        solve_param = [];
-        solve_user_order = [];
-        solve_func_order = [];
-        dyn_cons_param = [];
-        dyn_cons_user_order = [];
-        dyn_cons_func_order = [];
-        loc_cons_param = [];
-        loc_cons_user_order = [];
-        loc_cons_func_order = [];
+        solve = struct('param', [], 'user_order', [], 'func_order', []);
+        dyn_cons = struct('param', [], 'user_order', [], 'func_order', []);
+        loc_cons = struct('param', [], 'user_order', [], 'func_order', []);
+        external_func = 'jat';
     end
     
+    
     methods
-        function obj = solve_consider(solve, dyn_cons, loc_cons)
+        function obj = solve_consider(solve_in, dyn_cons_in, loc_cons_in, external_func_in)
             if nargin > 0
-                obj.solve_param = solve;
-                obj.dyn_cons_param = dyn_cons;
-                obj.loc_cons_param = loc_cons;
-            else
-                disp 'Need solve for and consider parameters'
+                % Provides the opportunity to define solve for/consider
+                % params when the class is created.
+                obj.solve.param = solve_in;
+                obj.dyn_cons.param = dyn_cons_in;
+                obj.loc_cons.param = loc_cons_in;
+                obj.external_func = external_func_in;
             end
             
-            if (length(solve_param) > 0)
-                solve_user_order = 1:length(solve_param);
+            if (~isempty(obj.solve.param))
+                obj.solve.user_order = 1:length(obj.solve.param);
             end
             
-            if (length(dyn_cons_param) > 0)
-                dyn_cons_user_order = 1:length(dyn_cons_param);
+            if (~isempty(obj.dyn_cons.param))
+                obj.dyn_cons.user_order = 1:length(obj.dyn_cons.param);
             end
             
-            if (length(loc_cons_param) > 0)
-                loc_cons_user_order = 1:length(loc_cons_param);
+            if (~isempty(obj.loc_cons.param))
+                obj.loc_cons.user_order = 1:length(obj.loc_cons.param);
             end
         end
         
+        
         function [xDot,A,Q] = extForces(obj,t,x,jatWorld)
-            
+            % Interface maintains compatibility with jatForces?
+            if (obj.external_func == 'jat')
+                
+            elseif (obj.external_func == 'gmat')
+                
+            else
+                disp 'External function not recognized.'
+            end
         end
+        
     end
     
 end

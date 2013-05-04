4869d7b05cd241b1036fc7ecb27ee8a6e68cf5f3
Ravi Mathur <ravi.mathur@emergentspace.com>
2013-05-04 02:13:32 -0400

    Mantis 435: Fully extracted estval regression tests to estval_test.m.

--- odtbx/Regression_Validation/estval_test.m
+++ odtbx/Regression_Validation/estval_test.m
@@ -21,11 +21,68 @@ function failed = estval_test
 %   Author      		Date         	Comment
 %
 %   Ravi Mathur         08/28/2012      Created
+%   Ravi Mathur         05/03/2013      Fully extracted regression test
+%                                       from original estval.m
 
-% The regression test for estval is deeply embedded within estval.m.
-% Removing it will require considerable effort. This wrapper
-% exists to conform to the new regression test framework.
+totaltests = 2;
 
-failed = estval;
+% Run all the test cases
+for k = 1:totaltests,
+    disp(['Case ',num2str(k),'...'])
+    fail(k) = run_test(k);
+end
+
+failed = any(fail);
+
+end %function
+
+% Runs the actual estval tests. The contents of this function have been
+% extracted from estval.
+function fail = run_test(testnum)
+
+% Common test parameters
+n = 19;
+N = 100;
+K = 10;
+randn('state', 0);
+e=randn(n,N,K);
+fs=e+1.5;
+t = linspace(0,100,N);
+Pt = [];
+
+% This test will be opening several figures, and we don't want them to
+% overwrite any existing figures. So find the largest open figure handle.
+% There may be figures with non-integer handles, e.g. if the 'publish'
+% command was recently used. We need to ignore these.
+allfigs = [get(0, 'children');0]; % Get all open figures
+badidx = find(allfigs ~= floor(allfigs)); % Find non-integer handles
+allfigs(badidx) = []; % Remove non-integer handles
+fhs = max(allfigs); % Get largest of remaining integer handles
+
+% Set test-dependent parameters
+load estval_test;
+switch testnum
+    case 1
+        iusemn = 0;
+        g_save = g_save0;
+        
+    case 2
+        iusemn = 1;
+        g_save = g_save1;
+end
+
+% Run estval test, getting plot info as the result
+h = estval(t, e, fs, Pt, fhs, iusemn);
+
+% Check results
+lh = length(h);
+fail = 0;
+for i = lh:-1:1
+    g(i) = get(h(i));
+    fail = any([fail ( any (abs(g(i).YData - g_save(i).YData) > eps) )]);
+end
+
+% Close generated figures
+close (fhs+1:gcf);
 
 end

411ba26b134a5edca8160505d9e346d8a16cfe7b
Phillip Anderson <phillip.anderson@emergentspace.com>
2013-05-09 16:04:47 -0600

    Changed preallocation of A (for speed). Modified hash references in if statements.

--- odtbx/ODTBX_Source/solve_consider.m
+++ odtbx/ODTBX_Source/solve_consider.m
@@ -79,8 +79,9 @@ classdef solve_consider
         function [xDot,A,Q] = obj.jatForces(obj,t,x,jatWorld)
             % John Gaebler's Code, revised
             [nx,nt] = size(x);
+            dyn_max = max(cell2mat(obj.dyn_cons.user_order));
             xDot = zeros(nx,nt);
-            A = zeros(nx,nx,nt);
+            A = zeros(nx,dyn_max,nt);
             Q = zeros(nx,nx,nt);
 
             [Fei,Fsi,Fmi,Fsri]=deal([]);
@@ -111,8 +112,6 @@ classdef solve_consider
             acc_moon=nan(3,nt);
 
             if( nargout > 1 )
-                A = [];
-
                 useGravPartial = ( jatWorld.get_j2_gravity_flag() || jatWorld.get_2body_gravity_flag() );
                 useDragPartial = jatWorld.get_drag_flag();
 
@@ -134,14 +133,14 @@ classdef solve_consider
                     %
                     % If a force is not added, all higher slots are shifted.
                     cur = 0;
-                    if isKey(obj.dyn_cons.user_map, 'EARTH-GM') % earth gravity error
+                    if Fei % earth gravity error
                         % need spacecraft position and earth GM
                         acc_earth(:,i) = ...
                             jatWorld.spacetime.getForce(cur).acceleration(jatWorld.spacetime.time,jatWorld.spacetime.earthRef,jatWorld.sc).getArray/1000;% m -> km
                         A(4:6,Fei,i) = A(4:6,Fei,i) + acc_earth(:,i);
                         cur=cur+1;
                     end
-                    if isKey(obj.dyn_cons.user_map, 'SOLAR-GM') % solar gravity error
+                    if Fsi % solar gravity error
                         % need spacecraft inertial position, sun inertial position,
                         % and sun GM
                         acc_sun(:,i) = ...
@@ -149,7 +148,7 @@ classdef solve_consider
                         A(4:6,Fsi,i) = A(4:6,Fsi,i) + acc_sun(:,i);
                         cur=cur+1;
                     end
-                    if isKey(obj.dyn_cons.user_map, 'LUNAR-GM') % lunar gravity error
+                    if Fmi % lunar gravity error
                         % need spacecraft inertial position, lunar inertial
                         % position, and lunar GM
                         acc_moon(:,i) = ...
@@ -161,7 +160,7 @@ classdef solve_consider
                         A(4:6,1:3,i) = A(4:6,1:3,i) + jatWorld.dragPartials();
                         cur=cur+1;
                     end
-                    if isKey(obj.dyn_cons.user_map, 'SOLRAD 8')
+                    if Fsri
                         SRPjat(:,i) = ...
                             jatWorld.spacetime.getForce(cur).acceleration(jatWorld.spacetime.time,jatWorld.spacetime.earthRef,jatWorld.sc).getArray;
                         A(4:6,Fsri,i) = A(4:6,Fsri,i) + (SRPjat(:,i))/1000;% m -> km          +[-3.05073589;6.75748831;2.03244886]*1e-9

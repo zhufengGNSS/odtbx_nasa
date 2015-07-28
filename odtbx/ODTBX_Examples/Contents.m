ce195f5b7d32b4972ee46e76fe9a04ccf3c3e2a7
Ravi Mathur <ravi.mathur@emergentspace.com>
2015-07-28 17:50:02 -0400

    R2015a prep: update documentation

--- odtbx/ODTBX_Examples/Contents.m
+++ odtbx/ODTBX_Examples/Contents.m
@@ -1,55 +1,55 @@
 % ODTBX Tutorials (can be used with the "publish" command)
 %
-%   pancake_tutorial             - OD Toolbox Tutorial 1: Planet Pancake
-%   batch_3gs_tutorial           - OD Toolbox Tutorial 2: Batch Estimation with Multiple Ground Stations
-%   chaser_target_tutorial       - OD Toolbox Tutorial 3: Chaser-Target Estimation with Multiple Dynamics/Measurement Models
+%   pancake_tutorial          - OD Toolbox Tutorial 1: Planet Pancake
+%   batch_3gs_tutorial        - OD Toolbox Tutorial 2: Batch Estimation with Multiple Ground Stations
+%   chaser_target_tutorial    - OD Toolbox Tutorial 3: Chaser-Target Estimation with Multiple Dynamics/Measurement Models
 %
 % ODTBX Examples
 %
-%   AutoDX_example               - An example of using AutoDX to compute optimal step sizes for finite-difference derivatives.
-%   batch_estimator_demo         - Batch Estimator Demo
-%   batch_example_1              - Example for the ODTBX Batch Estimator, estbat.m, using range and range rate measurements.
-%   batch_example_2              - Example for the ODTBX Batch Estimator, estbat.m, using GSMEAS measurements.
-%   batch_example_3              - Example for the ODTBX Batch Estimator, estbat.m, similar to batch_example_1 with no process noise.
-%   ddormeas_example             - This demonstrates the use of the ddor measurement model.
-%   example_estspf               - This file is an example for the ODTBX Sigma Point Filter Estimator, estspf.m, with Monte Carlo runs.
-%   example_estspf_2             - This file is an example for the ODTBX Sigma Point Filter Estimator, estspf.m. 
-%   example_estsrif              - Orbit determination of the IBEX mission using the Square Root Information Filter, ESTSRIF
-%   gsmeas_example               - This demonstrates the use of the gsmeas measurement model.
-%   gsmeas_linkbudget_example    - This demonstrates the use of the gsmeas measurement model.
-%   gps_antenna_tools_demo       - Demo for GPS link budget and antenna pattern analysis tools.
-%   gpsmeas_example              - An example script that demonstrates the use of the GPS measurement model, gpsmeas.
-%   lnrmeas_example              - This file is an example for the lunar relay measurement model, lnrmeas.m
-%   matlabjatinterface           - Example using the JAT RK8 Integrator and Matlab.
-%   opnavExample                 - Example to demonstrate the functionality of OPNAVMEAS and perform regression tests.
-%   sequential_estimator_demo    - Sequential Estimator Demo
-%   sequential_example_1         - This file is an example for the ODTBX Sequential Estimator, estseq.m
-%   sequential_example_2         - This file is an example for the ODTBX Sequential Estimator, estseq.m
-%   testMeasPartials_example     - This example tests the H matrix derivations of gsmeas, tdrssmeas, ddormeas, lnrmeas, and gpsmeas.
-%   tdrss_example1               - This file is an example for the tdrss measurement model, tdrssmeas.m
-%   tdrss_example2               - This file is an example for the tdrss measurement model, tdrssmeas.m
-%   tdrss_example3               - Input TDRS States as SPEphem type ephemeris files
-%   tdrss_basic_example3         - Input TDRS States as SPEphem type ephemeris files
-%   XNAV_only_LEO                - LEO Orbit Determination Simulation of XNAV
+%   AutoDX_example            - An example of using AutoDX to compute optimal step sizes for finite-difference derivatives.
+%   batch_estimator_demo      - Batch Estimator Demo
+%   batch_example_1           - Example for the ODTBX Batch Estimator, estbat.m, using range and range rate measurements.
+%   batch_example_2           - Example for the ODTBX Batch Estimator, estbat.m, using GSMEAS measurements.
+%   batch_example_3           - Example for the ODTBX Batch Estimator, estbat.m, similar to batch_example_1 with no process noise.
+%   ddormeas_example          - This demonstrates the use of the ddor measurement model.
+%   example_estspf            - This file is an example for the ODTBX Sigma Point Filter Estimator, estspf.m, with Monte Carlo runs.
+%   example_estspf_2          - This file is an example for the ODTBX Sigma Point Filter Estimator, estspf.m. 
+%   example_estsrif           - Orbit determination of the IBEX mission using the Square Root Information Filter, ESTSRIF
+%   gsmeas_example            - This demonstrates the use of the gsmeas measurement model.
+%   gsmeas_linkbudget_example - This demonstrates the use of the gsmeas measurement model.
+%   gps_antenna_tools_demo    - Demo for GPS link budget and antenna pattern analysis tools.
+%   gpsmeas_example           - An example script that demonstrates the use of the GPS measurement model, gpsmeas.
+%   lnrmeas_example           - This file is an example for the lunar relay measurement model, lnrmeas.m
+%   matlabjatinterface        - Example using the JAT RK8 Integrator and Matlab.
+%   opnavExample              - Example to demonstrate the functionality of OPNAVMEAS and perform regression tests.
+%   sequential_estimator_demo - Sequential Estimator Demo
+%   sequential_example_1      - This file is an example for the ODTBX Sequential Estimator, estseq.m
+%   sequential_example_2      - This file is an example for the ODTBX Sequential Estimator, estseq.m
+%   testMeasPartials_example  - This example tests the H matrix derivations of gsmeas, tdrssmeas, ddormeas, lnrmeas, and gpsmeas.
+%   tdrss_example1            - This file is an example for the tdrss measurement model, tdrssmeas.m
+%   tdrss_example2            - This file is an example for the tdrss measurement model, tdrssmeas.m
+%   tdrss_example3            - Input TDRS States as SPEphem type ephemeris files
+%   tdrss_basic_example3      - Input TDRS States as SPEphem type ephemeris files
+%   XNAV_only_LEO             - LEO Orbit Determination Simulation of XNAV
 %
 % Support Functions (used by tutorials & examples)
 %
-%   dualIADat                    - Get measurement values for the chaser_target tutorial.
-%   dualIADyn                    - Get dynamics & estimation values for the chaser_target tutorial.
-%   editratio_test               - Specify the initial reference state.
-%   gps_antenna_tools_support    - Support script to gps_antenna_tools_demo.m
-%   pancake_dat                  - Get measurement data for the pancake tutorial.
-%   pancake_datsf                - Get measurement data for the pancake tutorial.
-%   pancake_dynoi                - Get dynamics & estimation values for the pancake tutorial.
-%   pancake_dynsf                - Get dynamics & estimation values for the pancake tutorial.
-%   pancake_dyn                  - Get dynamics & estimation values for the pancake tutorial.
-%   plot_results                 - Plots estimator error, measurment residuals, variance sandpiles 
-%   plot_results_ric             - Plots estimator error, measurment residuals, variance sandpiles in the Radial-In-track-Cross-track directions.
-%   pr2bp                        - Planar restricted two-body problem dynamics equations.
-%   r2bp                         - Restricted Two-body problem dynamics equations.
-%   rrdot3D_1way                 - Range and range-rate measurement model for 3-D inertial state
-%   range2D                      - Range measurement model for 2-D inertial state space.
-%   rrdot2D                      - Range and range-rate measurement model for 2-D inertial state
-%   rrdot3D                      - Range and range-rate measurement model for 3-D inertial state
-%   testeom                      - Example EOM file for use with the MATLAB Adaptor.
-%   upvec_test                   - estseq example.
+%   dualIADat                 - Get measurement values for the chaser_target tutorial.
+%   dualIADyn                 - Get dynamics & estimation values for the chaser_target tutorial.
+%   editratio_test            - Specify the initial reference state.
+%   gps_antenna_tools_support - Support script to gps_antenna_tools_demo.m
+%   pancake_dat               - Get measurement data for the pancake tutorial.
+%   pancake_datsf             - Get measurement data for the pancake tutorial.
+%   pancake_dynoi             - Get dynamics & estimation values for the pancake tutorial.
+%   pancake_dynsf             - Get dynamics & estimation values for the pancake tutorial.
+%   pancake_dyn               - Get dynamics & estimation values for the pancake tutorial.
+%   plot_results              - Plots estimator error, measurment residuals, variance sandpiles 
+%   plot_results_ric          - Plots estimator error, measurment residuals, variance sandpiles in the Radial-In-track-Cross-track directions.
+%   pr2bp                     - Planar restricted two-body problem dynamics equations.
+%   r2bp                      - Restricted Two-body problem dynamics equations.
+%   rrdot3D_1way              - Range and range-rate measurement model for 3-D inertial state
+%   range2D                   - Range measurement model for 2-D inertial state space.
+%   rrdot2D                   - Range and range-rate measurement model for 2-D inertial state
+%   rrdot3D                   - Range and range-rate measurement model for 3-D inertial state
+%   testeom                   - Example EOM file for use with the MATLAB Adaptor.
+%   upvec_test                - estseq example.
